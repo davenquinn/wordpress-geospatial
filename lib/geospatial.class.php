b6a58e5bf735c60c19c91509d56d7f9a193863b0
Daven Quinn <davenquinn@gmail.com>
2012-08-11 02:25:57 -0400

    Moved OpenLayers to new home in js directory

--- lib/geospatial.class.php
+++ lib/geospatial.class.php
@@ -1,13 +1,31 @@
 <?php
+
 class Geospatial {
 
 	function __construct() {
 		add_action( 'init', array(&$this, '_create_types'));
+		add_action( 'init', array(&$this, 'enqueue_scripts'));
 		add_filter('the_posts', array(&$this, '_filter_the_posts'));
 
 		$this->admin = new GeospatialAdmin($this);
 
 	}
+
+	static function status_message($message, $error=false) {
+		return array(
+				"error" => $error,
+				"message" => $message
+			);
+	}
+
+	function enqueue_scripts () {
+		wp_register_script('openlayers', GEOSPATIAL_URL . "js/openlayers/OpenLayers.js");
+    	wp_enqueue_script('geospatial', GEOSPATIAL_URL . "js/geospatial.js", array("openlayers", "jquery"));
+    	wp_enqueue_style('global-style', GEOSPATIAL_URL . "css/global.css");
+
+    	wp_enqueue_style('openlayers-style', GEOSPATIAL_URL . "js/openlayers/theme/default/style.css");
+
+    }
 
 	function _create_types() {
 
@@ -16,13 +34,13 @@ class Geospatial {
 			'singular_label' => __('Place'),
 			'public' => true,
 			'show_ui' => true,
-			'taxonomies' => array('geotype','tag','category'),
+			'taxonomies' => array('geotype'),
 			'capability_type' => 'post',
 			'hierarchical' => false,
 			'has_archive' => false,
 			'rewrite' => false, ///array('slug'=>'geo', 'with_front'=>false),
 			'query_var' => true,
-			'supports' => array('editor', 'author', 'custom-fields', 'geojson_meta')
+			'supports' => array('title', 'author', 'custom-fields', 'geojson_meta')
 		));
 
 
