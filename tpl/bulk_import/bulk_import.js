ca538b5c6a3b81cee44edb3fe754861f1dcd5b0f
Daven Quinn <davenquinn@gmail.com>
2012-08-11 02:28:10 -0400

    Initial files for bulk import functionality.

--- tpl/bulk_import/bulk_import.js
+++ tpl/bulk_import/bulk_import.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($){
-	$('#importPanel div').hide();
+	/* $('#importPanel div').hide();
 	$('#importPanel div:first').show();
 	$('#importPanel ul li:first').addClass('active');
 	var currentTab = $('#importPanel ul li:first').attr('href');
@@ -12,6 +12,55 @@ jQuery(document).ready(function($){
 		$('#importPanel div').hide();
 		$(currentTab).show();
 		return false;
-	});
+	}); */
+	$('input#bulk_import_submit').click(function(){
+		nonce = $('input#geospatial_noncename').val();
+	    $.ajax({
+	        url: ajaxurl,
+	         data: { 
+	            action: 'geospatial-start-import',
+	            nonce: nonce
+	         },
+	         type: 'post',
+	         success: function(output) {
+	         	output = JSON.parse(output);
+	         	$('#bulk_import').addClass("validate").html("<div id='alert_panel'></div><div id='features_preview'></div>");
+	         	$('#features_preview').html(output.data);
+	            $('#alert_panel').html(output.status.message);
+	            $('#alert_panel').removeClass()
+	            if(output.status.error) {
+	            	$('#alert_panel').addClass('error');
+	            } else {
+	            	$('#alert_panel').addClass('ok');
+	            }
+
+	            on_validate();
+	        }
+	    });
+	    return false;
+    });
+
+function on_validate() {
+    // Validation functions
+    $('input[name=validate_submit]').click(function() {
+		nonce = $('input#geospatial_noncename').val();
+	    $.ajax({
+	        url: ajaxurl,
+	         data: { 
+	            action: 'geospatial-finish-import',
+	            nonce: nonce
+	         },
+	         type: 'post',
+	         success: function(output) {
+	         	output = JSON.parse(output);
+	        }
+	    });
+	    return false;   	
+    });
+    $('input[name=validate_cancel]').click(function() {
+	    location.reload();
+	    return true;  	
+    });
+}
 
 });
--- tpl/bulk_import/bulk_import.php
+++ tpl/bulk_import/bulk_import.php
@@ -1,33 +1,35 @@
 <div class="wrap">
 	<h2>Bulk Import Geospatial Data</h2>
-	<form id="importPanel">
-		<ul>
-			<li><a href="#url-field">URL from the web</a></li>
-			<li><a href="#upload-field">File to upload</a></li>
-		</ul>
-		<div id="url-field">
-			<input type="text" name="url" />
-		</div>
-		<div id="upload-field">
-			<input type="file" name="upload" />
-		</div>
-		<input type="submit" value="Go" />
+	<div id="bulk_import">
+		<form id="importPanel">
+			<ul>
+				<li><a href="#url-field">URL from the web</a></li>
+				<li><a href="#upload-field">File to upload</a></li>
+			</ul>
+			<div id="url-field">
+				<input type="text" name="url" />
+			</div>
+			<div id="upload-field">
+				<input type="file" name="upload" />
+			</div>
+			<input id="bulk_import_submit" type="button" value="Go" />
 
-	</form>
-	<div id="geospatial_bulk_import_options" class="postbox">
-		<h3>Options</h3>
-		<div class="inside">
-			<form>
-				<label for="baseName">Base Name for Imported Features</label><br/>
-				<input type="text" name="baseName" />
-				<fieldset>
-					<legend>GeoJSON import</legend>
-					<input type="checkbox" name="includeTitleDate" />
-					<label for="includeTitleDate">Try to find title and date</label><br/>
-					<input type="checkbox" name="includeAttrs" />
-					<label for="includeAttrs">Include attributes as custom fields</label>
-				</fieldset>
-			</form>
+		</form>
+		<div id="geospatial_bulk_import_options" class="postbox">
+			<h3>Options</h3>
+			<div class="inside">
+				<form>
+					<label for="baseName">Base Name for Imported Features</label><br/>
+					<input type="text" name="baseName" />
+					<fieldset>
+						<legend>GeoJSON import</legend>
+						<input type="checkbox" name="includeTitleDate" />
+						<label for="includeTitleDate">Try to find title and date</label><br/>
+						<input type="checkbox" name="includeAttrs" />
+						<label for="includeAttrs">Include attributes as custom fields</label>
+					</fieldset>
+				</form>
+			</div>
 		</div>
 	</div>
 	<?php wp_nonce_field('geospatial_bulk_import_nonce','geospatial_noncename'); ?>
